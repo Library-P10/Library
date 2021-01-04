7a51cf722911be45528ee083b46863e630c9a495
Brichet <brichet.b@hotmail.fr>
2021-01-04 12:07:22 +0100

    Début de mise en place de récupération des listes d'attente via le get Bookg by Id

--- api/model/src/main/java/com/api/library/model/WaitingList.java
+++ api/model/src/main/java/com/api/library/model/WaitingList.java
@@ -19,22 +19,36 @@ public class WaitingList {
     @JoinColumn(name = "customer_id")
     private Customer customer;
 
+    @ManyToOne
+    @JoinColumn(name = "book_id")
+    private Book book;
+
     @Override
     public String toString() {
         return "WaitingList{" +
                 "id=" + id +
                 ", dateRequest=" + dateRequest +
                 ", customer=" + customer +
+                ", book=" + book +
                 '}';
     }
 
     public WaitingList() {
     }
 
-    public WaitingList(final Long id, final Date dateRequest, final Customer customer) {
+    public WaitingList(final Long id, final Date dateRequest, final Customer customer, final Book book) {
         this.id = id;
         this.dateRequest = dateRequest;
         this.customer = customer;
+        this.book = book;
+    }
+
+    public Book getBook() {
+        return book;
+    }
+
+    public void setBook(final Book book) {
+        this.book = book;
     }
 
     public Long getId() {
